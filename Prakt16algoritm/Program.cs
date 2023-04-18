023de30facdeef7af9f0e137f22eec7c7e04f610
Avondeil <123551851+Avondeil@users.noreply.github.com>
2023-04-18 15:36:49 +0500

    Add files via upload

---
+++ Prakt16algoritm/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt16algoritm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool scan = true;
+            Console.Write("Введите строку s1: "); string s1 = Console.ReadLine();
+            Console.Write("Введите строку s2: "); string s2 = Console.ReadLine();
+            Stack <char> stack = new Stack<char>();
+            foreach (char c in s1)
+            {
+                stack.Push(c);
+            }
+            foreach (char c in s2)
+            {
+                if (stack.Count == 0 || c != stack.Pop())
+                {
+                    scan = false;
+                    break;
+                }
+            }
+            if (scan)
+            {
+                Console.WriteLine("s2 является обратной s1");
+            }
+            else
+            {
+                Console.WriteLine("s2 не является обратной s1");
+            }
+            Console.WriteLine("Нажмите любую кнопку для выхода из программы");
+            Console.ReadKey();
+            
+        }
+    }
+}
